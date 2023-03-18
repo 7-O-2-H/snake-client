72df9a4650e5e18bcae5a9dacab5c9bead66db80
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-18 19:59:22 -0400

    input

--- input.js
+++ input.js
@@ -1,35 +1,46 @@
+const { clearInterval } = require("timers");
+
 let connection;
 
-const handleUserInput = function (key) {
+let intervalID = null;
+let moveHolder = null;
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    setInterval(() => {
-      connection.write('Move: up');
-    }, 200);
-  };
-  if (key === 'a') {
-    setInterval(() => {
-      connection.write('Move: left');
-    }, 200);
-  };
-  if (key === 's') {
-    setInterval(() => {
-      connection.write('Move: down');
-    }, 200);
+  
+  let move = null;
+  
+  if (key === 'w' && moveHolder !== 's') {
+    move = 'Move: up';
+    moveHolder = key;
   }
-  if (key === 'd') {
-    setInterval(() => {
-      connection.write('Move: right');
-    }, 200);
+  if (key === 'a' && moveHolder !== 'd') {
+    move = 'Move: left';
+    moveHolder = key;
+  }
+  if (key === 's' && moveHolder !== 'w') {
+    move = 'Move: down';
+    moveHolder = key;
+  }
+  if (key === 'd' && moveHolder !== 'a') {
+    move = 'Move: right';
+    moveHolder = key;
+    
   }
   if (key === 't') {
     connection.write('Say: Nommin');
   }
+  if (move) {
+    clearInterval(intervalID);
+    intervalID = null;
+    intervalID = setInterval(() => {
+      connection.write(move);
+    }, 90);
+  }
 };
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
